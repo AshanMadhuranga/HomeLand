d5bd1ee63500cbe6629bfb83240a1d7d358ee8ce
AshanMadhuranga <ashanmadhuranga48@gmail.com>
2026-09-15 00:30:07 +0530

    modified

--- src/main/java/com/landhub/HomeController.java
+++ src/main/java/com/landhub/HomeController.java
@@ -10,14 +10,4 @@ public class HomeController {
     public String home() {
         return "index";
     }
-
-    @GetMapping("/lands")
-    public String lands() {
-        return "lands";
-    }
-
-    @GetMapping("/lands/{id}")
-    public String landDetails() {
-        return "land-details";
-    }
 }
